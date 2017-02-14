05ea5fdb01f548fedd8b9f503a5638c8aed2f0a1
Aditi096 <pixie.aditi0096@gmail.com>
2017-02-14 13:26:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,33 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-/*var content = {
-  title : articleOne,
-  heading : Death Note,
+var content = {
+  title : 'articleOne',
+  heading : 'Death Note',
   content : 'WE ARE ALL EQUAL IN THE EYES OF THE REAPER.'
-};*/
+};
+
+var htmlTemplate = {
+    <html>
+    <head>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="center">
+            <img src="https://img1.etsystatic.com/154/1/9820149/il_340x270.1141813627_brud.jpg" class="img-medium"/>
+        </div>
+        <br>
+        <div class="center text-big bold">
+            Working hard is important.<br> 
+            But there is something that matters more:<br>
+            <h1>BELIEVING IN YOURSELF</h1>
+                        -Harry Potter
+        </div>
+        <script type="text/javascript" src="/ui/main.js">
+        </script>
+    </body>
+</html>
+};
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
